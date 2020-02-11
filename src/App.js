a51cd0169c8576170022cd17045235b95fae1e52
ismail <ismail.elydi@gmail.com>
2020-02-11 12:16:10 +0100

    Set up Env

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import Login from './compenent/Login';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+          <Login/>
+      </Fragment>
+    )
+  }
+}
+
